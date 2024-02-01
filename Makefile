3a01d826d50ea6a3449f6516ed074529a6c0912a
Eduardo Corona <eduardo.corona040@gmail.com>
2024-02-01 01:45:10 +0000

    first commit

---
+++ Makefile
@@ -0,0 +1,39 @@
+NAME = fdf
+
+SRCS = test.c
+
+OBJS = $(SRCS:.c=.o)
+
+CC = cc
+
+CFLAGS = -Wall -Wextra -Werror
+
+LIBFT = libft
+
+LIBMLX = minilibx-linux
+
+MLXFLAGS = -I$(LIBMLX) -L$(LIBMLX) -L/usr/lib -lmlx_Linux -lXext -lX11
+
+FTFLAGS = -I$(LIBFT) -L$(LIBFT) -lft
+
+all: $(NAME)
+
+$(NAME): $(OBJS) $(LIBFT)/libft.a
+	$(CC) $(CFLAGS) $(OBJS) $(MLXFLAGS) $(FTFLAGS) -lm -o $@
+
+$(LIBFT)/libft.a:
+	@make -C libft printf
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $<
+
+clean:
+	rm -rf $(OBJS)
+	make -C libft clean
+
+fclean: clean
+	rm -rf $(NAME) $(LIBFT)/libft.a
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ fdf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecorona- <ecorona-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 00:50:14 by ecorona-          #+#    #+#             */
+/*   Updated: 2024/02/01 01:42:44 by ecorona-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+#define FDF_H
+
+# include "X11/X.h"
+# include "minilibx-linux/mlx.h"
+# include "libft/libft.h"
+# include "libft/ft_printf.h"
+
+# define XK_Escape 0xff1b
+
+struct window
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+};
+
+#endif
